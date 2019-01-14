a4bbcafa5a850741ad30fdd8e23ae8aa030b9d24
enzosanci <enzo-sanci@etud.univ-tln.fr>
2019-01-14 16:59:48 +0100

    add modifs

--- newcode.py
+++ newcode.py
@@ -63,8 +63,10 @@ def verif_fus(point1, point2, tracker):
 				trackers.remove(tracker)
 				if state_var == 0:
 					print("Fusion detectee")
+					return(1)
 				else:
 					print("Separation detectee")
+					return(1)
 				cpt_frame = 0
 		else:
 			cpt_frame = 0
@@ -86,6 +88,7 @@ stock_frame(frame_list, args["video"])
 reverse_frame_list = copy.deepcopy(frame_list)
 reverse_frame_list.reverse()
 
+print(len(frame_list))
 
 for frame in frame_list:
 
@@ -96,9 +99,9 @@ for frame in frame_list:
 		if success:
 			(x, y, w, h) = [int(v) for v in boxe]
 			point = [x+(w//2), y+(h//2)]
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-			cv2.putText(frame, "Personne "+str(trackers.index(tracker)) , (x, y-2), cv2.FONT_HERSHEY_SIMPLEX,
-						0.3, (0,255,0), 1, cv2.LINE_AA)
+			cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
+			cv2.putText(frame, "Filament "+str(trackers.index(tracker)) , (x, y-2), cv2.FONT_HERSHEY_SIMPLEX,
+						0.3, (255,255,255), 1, cv2.LINE_AA)
 			verif_fus(point, old_point, tracker)
 			old_point = point
 
@@ -117,21 +120,25 @@ for frame in frame_list:
 
 trackers = []
 state_var = 1
-time.sleep(5)
-
+time.sleep(2)
+z = []
+counter = 0
 for frame in reverse_frame_list:
-
+	
 	old_point = None
 
 	for tracker in trackers:
+		
 		(success, boxe) = tracker.update(frame)
 		if success:
 			(x, y, w, h) = [int(v) for v in boxe]
 			point = [x+(w//2), y+(h//2)]
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-			cv2.putText(frame, "Personne "+str(trackers.index(tracker)) , (x, y-2), cv2.FONT_HERSHEY_SIMPLEX,
-						0.3, (0,255,0), 1, cv2.LINE_AA)
-			verif_fus(point, old_point, tracker)
+			cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 255), 2)
+			cv2.putText(frame, "Filament "+str(trackers.index(tracker)) , (x, y-2), cv2.FONT_HERSHEY_SIMPLEX,
+						0.3, (255,255,255), 1, cv2.LINE_AA)
+			if verif_fus(point, old_point, tracker) == 1:
+				#Enregistrer les frames du debut de la liste jusqu a la frame courante
+				z = copy.deepcopy(reverse_frame_list[:counter])
 			old_point = point
 
 	cv2.imshow("Frame", frame)
@@ -145,6 +152,28 @@ for frame in reverse_frame_list:
 		trackers.append(tracker)
 	elif key == ord("q"):
 		break
-
 	tick(fps)
+	counter +=1
+
+z.reverse()
+taille = len(z)
+frame_list_size = len(frame_list)
+final_list = frame_list[:frame_list_size - taille]
+final_list += z
+
+
+#print(final_list)
+
+video = cv2.VideoWriter("sortie.avi", 0, 30, (600, 600))
+
+
+
+for frame in final_list:
+	cv2.imshow("Frame", frame)
+	#time.sleep(0.1)
+	cv2.waitKey(25)
+	video.write(frame)
+
+video.release()	
+
 cv2.destroyAllWindows()
